6ac07c7ada862ba072c213c78bed691915a8fb7d
Khoa Phan <pldkhoa96.work@gmail.com>
2022-10-30 13:46:35 +0700

    add intro section

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Intro = () => {
-    return <div>Intro</div>;
+    return (
+        <section className="section bg-raisin-black text-white">
+            <div className="container text-center w-3/4 max-w-2xl">
+                <h2 className="mb-5 lg:mb-10">Welcome</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                    alias aliquam beatae blanditiis cumque doloremque fugiat
+                    inventore ipsa minima, necessitatibus nulla provident sint
+                    tenetur velit.
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default Intro;
