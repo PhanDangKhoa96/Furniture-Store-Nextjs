9afe85064b57cbcac0312bf3efd5d00c66f24e0f
Khoa Phan <pldkhoa96.work@gmail.com>
2022-11-02 19:59:08 +0700

    add increase quantity function

--- components/Cart/CartItem.js
+++ components/Cart/CartItem.js
@@ -6,11 +6,15 @@ import Link from 'next/link';
 import { useStoreContext } from '../../store/context/store';
 import { actionType } from '../../store/reducer/storeReducer';
 
-const CartItem = ({ slug, image, title, _id }) => {
+const CartItem = ({ slug, image, title, _id, quantity }) => {
     const { dispatch } = useStoreContext();
 
     const removeProduct = (id) => {
         dispatch({ type: actionType.REMOVE_PRODUCT, payload: id });
+    };
+
+    const increaseQuantity = (id) => {
+        dispatch({ type: actionType.INCREASE_QUANTITY, payload: id });
     };
     return (
         <div className="-m-3 flex items-center rounded-lg p-2">
@@ -27,14 +31,25 @@ const CartItem = ({ slug, image, title, _id }) => {
             <div className="ml-4 flex-auto">
                 <Link href={`/product/${slug}`}>
                     <p className="text-sm font-medium text-gray-900">
-                        {title} x quantity
+                        {title} x {quantity}
                     </p>
                 </Link>
 
-                <input
-                    type="number"
-                    className="border border-amber-700 border-solid"
-                />
+                <div className="flex gap-x-2">
+                    <input
+                        type="text"
+                        value={quantity}
+                        className="border border-amber-700 border-solid"
+                    />
+
+                    <button
+                        className="text-lg"
+                        onClick={() => {
+                            increaseQuantity(_id);
+                        }}>
+                        +
+                    </button>
+                </div>
             </div>
 
             <button
--- store/reducer/storeReducer.js
+++ store/reducer/storeReducer.js
@@ -5,7 +5,8 @@ export const actionType = {
     ADD_TO_CART: 'ADD_TO_CART',
     INIT_STORE: 'INIT_STORE',
     REMOVE_PRODUCT: 'REMOVE_PRODUCT',
-    EMPTY_CART: 'EMPTY_CART'
+    EMPTY_CART: 'EMPTY_CART',
+    INCREASE_QUANTITY: 'INCREASE_QUANTITY'
 };
 
 const StoreReducer = (state, { type, payload }) => {
@@ -13,10 +14,11 @@ const StoreReducer = (state, { type, payload }) => {
         case actionType.ADD_TO_CART: {
             const product = state.find((item) => item._id === payload._id);
             if (product) {
-                setLocalstorage(state);
-                return state;
+                product.quantity++;
+                setLocalstorage([...state]);
+                return [...state];
             }
-            const newCart = [...state, payload];
+            const newCart = [...state, { ...payload, quantity: 1 }];
             setLocalstorage(newCart);
             return newCart;
         }
@@ -29,12 +31,18 @@ const StoreReducer = (state, { type, payload }) => {
             return [];
         }
         case actionType.REMOVE_PRODUCT: {
-            const newCart = state.filter(
-                (product) => product._id !== payload
-            );
+            const newCart = state.filter((product) => product._id !== payload);
 
             setLocalstorage(newCart);
             return newCart;
+        }
+        case actionType.INCREASE_QUANTITY: {
+            const product = state.find((item) => item._id === payload);
+            console.log(product);
+            const newCart = state.filter((product) => product._id !== payload);
+            product.quantity++;
+            setLocalstorage([...state]);
+            return [...state];
         }
     }
     return state;
